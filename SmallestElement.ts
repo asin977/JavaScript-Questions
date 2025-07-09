be7d67aa6676ccc170cf4f0f6a6d25e62b0525fe
Assin Anna <assinannameckal@gmail.com>
2025-07-09 17:59:39 +0530

    finding the smallest number

---
+++ SmallestElement.ts
@@ -0,0 +1,12 @@
+// Write a function to get second smallest element in an array.
+
+const getSecondSmallestElement = (array: number[]): number | undefined => {
+  if (array.length < 2) return undefined;
+
+  const smallestNumber = [...new Set(array)].sort((a, b) => a - b);
+
+  return smallestNumber[1];
+};
+
+console.log(getSecondSmallestElement([4, 1, 7, 1, 3, 9]));
+console.log(getSecondSmallestElement([5])); 
