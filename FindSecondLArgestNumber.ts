a3cffbdd3ff6382c8ad72588b3642378a92f8cfd
Assin Anna <assinannameckal@gmail.com>
2025-07-09 10:11:22 +0530

    find the second largest number in an array

---
+++ FindSecondLArgestNumber.ts
@@ -0,0 +1,15 @@
+const getSecondLargestNumber = (array: number[]): number | string => {
+  const findUniqueNumbers: number[] = [...new Set(array)];
+
+  if (findUniqueNumbers.length < 2) {
+    return "There is no second largest number";
+  }
+
+  findUniqueNumbers.sort((a: number, b: number): number => b - a);
+
+  return findUniqueNumbers[1];
+};
+
+console.log(getSecondLargestNumber([1, 5, 7, 8, 79, 45]));
+console.log(getSecondLargestNumber([10, 10, 10]));
+console.log(getSecondLargestNumber([5]));
