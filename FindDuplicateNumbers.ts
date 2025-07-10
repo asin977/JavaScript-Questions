357bf7e7910b1ae10f739a77c7b86ac6cbbb900d
Assin Anna <assinannameckal@gmail.com>
2025-07-10 20:45:39 +0530

    refractor:resolved the review comments

--- FindDuplicateNumbers.ts
+++ FindDuplicateNumbers.ts
@@ -1,9 +1,22 @@
-// Write a function to remove duplicates from an array.
-
 const removeDuplicates = (array: number[]): number[] => {
-  const findDuplicateNumbers: number[] = [...new Set(array)];
+  const result: number[] = [];
 
-  return findDuplicateNumbers;
+  for (let i = 0; i < array.length; i++) {
+    let isDuplicate = false;
+
+    for (let j = 0; j < result.length; j++) {
+      if (array[i] === result[j]) {
+        isDuplicate = true;
+        break;
+      }
+    }
+
+    if (!isDuplicate) {
+      result.push(array[i]);
+    }
+  }
+
+  return result;
 };
 
 console.log(removeDuplicates([1, 1, 2, 8, 5, 5, 6, 8, 10]));
