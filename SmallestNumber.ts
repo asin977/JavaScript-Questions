0a5ad57a31dce954dd22e23953be299101bcb5fa
Assin Anna <assinannameckal@gmail.com>
2025-07-10 22:09:19 +0530

    refractor:updated the code

--- SmallestNumber.ts
+++ SmallestNumber.ts
@@ -1,11 +1,25 @@
 // Write a function to get second smallest element in an array.
 
-const getSecondSmallestNumber = (array: number[]): number | undefined => {
-  if (array.length < 2) return undefined;
+const getSecondSmallestNumber = (array) => {
+  if (array.length < 2) console.log("Array must contain at least two elements");
 
-  const smallestNumber = [...new Set(array)].sort((a, b) => a - b);
+  let smallest = array[0];
 
-  return smallestNumber[1];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < smallest) {
+      smallest = array[i];
+    }
+  }
+
+  let secondSmallest = Infinity;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== smallest && array[i] < secondSmallest) {
+      secondSmallest = array[i];
+    }
+  }
+
+  return secondSmallest;
 };
 
 console.log(getSecondSmallestNumber([4, 1, 7, 1, 3, 9]));
