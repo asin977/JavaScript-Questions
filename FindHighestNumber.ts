5a6c628c70a51a22645fd4c47d13628a45fe6a57
Assin Anna <assinannameckal@gmail.com>
2025-07-10 20:12:47 +0530

    refrcator:resolved the review comments

--- FindHighestNumber.ts
+++ FindHighestNumber.ts
@@ -1,8 +1,17 @@
 // Write a function to find the largest number in an array.
 
 const getLargestNumber = (array) => {
-  if (array.length === 0) return "array is empty";
-  return Math.max(...array);
+  if (array.length === 0) console.log("array is empty");
+
+  let largest = array[0];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > largest) {
+      largest = array[i];
+    }
+  }
+
+  return largest;
 };
 
 console.log(getLargestNumber([5, 2, 8, 7, 9, 45]));
