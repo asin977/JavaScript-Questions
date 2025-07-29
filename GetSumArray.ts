fda0ec12b28955202fcaa01cba0d20ca892549d5
Assin Anna <assinannameckal@gmail.com>
2025-07-29 18:06:46 +0530

    feat:implemented a function to find the sum of an array

--- GetSumArray.ts
+++ GetSumArray.ts
@@ -1,15 +1,15 @@
 //Write a Program to find a sum of an array?
 
-function sumArray(numbers: number[]): number {
+const GetsumArray = (numbers: number[]): number => {
   let sum = 0;
   for (let num of numbers) {
     sum += num;
   }
   return sum;
-}
+};
 
-const nums: number[] = [5, 10, 15, 20, 25];
-const total = sumArray(nums);
+const nums: number[] = [10, 12, 20, 29, 28];
+const total = GetsumArray(nums);
 
 console.log("Array:", nums);
 console.log("Sum of array:", total);
