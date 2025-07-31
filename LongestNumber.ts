515b4eae98e156af096be8bb5492d1119559b26f
Assin Anna <assinannameckal@gmail.com>
2025-07-31 09:27:25 +0530

    refrcator:resolved the review comments

--- LongestNumber.ts
+++ LongestNumber.ts
@@ -1,22 +1,20 @@
 // Find the longest word in a string
 
-let longestWord = "";
-let currentWord = "";
+const getLongestWord = (str: string): string => {
+  let longestWord = "";
+  let currentWord = "";
 
-const GetLongestWord = (string) => {
-    for (let i=0;i<=string.length;i++) {
-        const char = string[i] || '';
-        if ((char >= 'a' && char <= 'z') || (char >='A' && char <='z')) {
-            currentWord += char;
-        }else {
-            if (currentWord.length > longestWord.length) {
-                longestWord = currentWord;
-            }
-            currentWord = '';
-        }
+  for (let i = 0; i <= str.length; i++) {
+    const code = str.charCodeAt(i) || 0;
+    if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+      currentWord += str[i];
+    } else {
+      if (currentWord.length > longestWord.length) longestWord = currentWord;
+      currentWord = "";
     }
-    return longestWord;
-}
+  }
+  return longestWord;
+};
 
-const sentence:string = 'The people in the bangalore city are always busy'
-console.log(GetLongestWord(sentence));
+const sentence: string = "The people in the bangalore city are always busy";
+console.log(getLongestWord(sentence));
